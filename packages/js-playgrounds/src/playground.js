a8a6afe925beda243d808ebec988259e9b12cfa8
Gal Schlezinger <gal@spitfire.co.il>
2018-10-23 14:26:32 +0300

    remove uuid and support more locations

--- packages/js-playgrounds/src/playground.js
+++ packages/js-playgrounds/src/playground.js
@@ -1,14 +1,16 @@
-const uuid = require('uuid')
-const {inspect} = require('util')
+const { inspect } = require("util");
 
-const exprs = []
+const exprs = [];
 
-process.on('exit', () => {
-  const results = JSON.stringify(exprs, null, 2).replace(/^/gm, "@PLAYGROUND: ")
-  console.log(results)
-})
+process.on("exit", () => {
+  const results = JSON.stringify(exprs, null, 2).replace(
+    /^/gm,
+    "@PLAYGROUND: "
+  );
+  console.log(results);
+});
 
 module.exports = (info, expr) => {
-  exprs.push({loc: info, expr: inspect(expr, false, 1, false) })
+  exprs.push({ loc: info, expr: inspect(expr) });
   return expr;
-}
+};
